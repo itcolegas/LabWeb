7fe05c46c91392ece27780ef83d6a65e471fcbfd
CristopherCM <cristophercejudo@Cristophers-MacBook-Pro.local>
2020-09-28 21:57:06 -0500

    Baloobot

---
+++ Baloobot/Backend/mlab.py
@@ -0,0 +1,32 @@
+import sys
+import pymongo
+uri = "mongodb+srv://dbCristopher:dbpinguino@cluster0.z5rj5.mongodb.net/cluster0?retryWrites=true&w=majority"
+###############################################################################
+# main
+###############################################################################
+def main(args):    
+    client = pymongo.MongoClient(uri)
+
+    db = client.get_default_database()    
+
+    songs = db['songs']    
+
+    SEED_DATA = [
+        {        'decade': '1970s',        'artist': 'Debby Boone',        'song': 'You Light Up My Life',        'weeksAtOne': 10    },    {        'decade': '1980s',        'artist': 'Olivia Newton-John',        'song': 'Physical',        'weeksAtOne': 10    },    {        'decade': '1990s',        'artist': 'Mariah Carey',        'song': 'One Sweet Day',        'weeksAtOne': 16    }]
+
+    songs.insert_many(SEED_DATA)   
+
+    query = {'song': 'One Sweet Day'}  
+
+    songs.update(query, {'$set': {'artist': 'Mariah Carey ft. Boyz II Men'}})   
+
+    cursor = songs.find({'weeksAtOne': {'$gte': 10}}).sort('decade', 1)    
+
+    for doc in cursor:        
+        print ('In the %s, %s by %s topped the charts for %d straight weeks.' %               
+        (doc['decade'], doc['song'], doc['artist'], doc['weeksAtOne']))   
+         
+        client.close()
+
+if __name__ == '__main__':    
+    main(sys.argv[1:])
