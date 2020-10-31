95f920e26a19346b65394fe7e405fa4bdc25d66f
CristopherCM <cristophercejudo@Cristophers-MacBook-Pro.local>
2020-10-30 20:36:50 -0600

    ultima y nos vamos

--- flemet/frontend/src/components/BalooBot/Chat.js
+++ flemet/frontend/src/components/BalooBot/Chat.js
@@ -3,6 +3,7 @@ import { Widget, renderCustomComponent } from "react-chat-widget";
 import 'react-chat-elements/dist/main.css';
 //import { MessageBox } from 'react-chat-elements'
 import ReactHtmlParser from 'react-html-parser'
+import Carousel from 'react-bootstrap/Carousel'
 import axios from 'axios';
 
 import './chat.css'
@@ -11,11 +12,57 @@ class Cheers extends React.Component{
   render() {
     return (
       <div className='speech-bubble'>
-        {ReactHtmlParser(this.props.text)}
+        {ReactHtmlParser(this.props.text)}     
       </div>
+      
     );
   }
 }
+
+class Servicios extends React.Component{
+  render() {
+    return (
+      <div className='speech-bubble'>
+        {ReactHtmlParser(this.props.text)}
+        <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/images/transporte.jpg"
+      alt="tranporte"
+    />
+    <Carousel.Caption>
+      <h1>Transporte</h1>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/images/almacenaje.jpg"
+      alt="almacenaje"
+    />
+    <Carousel.Caption>
+      <h1>Almacenaje</h1>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="/images/consultoria.png"
+      alt="consultoria"
+    />
+
+    <Carousel.Caption>
+      <h1>Consultoria</h1>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+      </div>
+      
+    );
+  }
+}
+
 
 class Chat extends React.Component {
   handleNewUserMessage = (newMessage) => {
@@ -26,7 +73,11 @@ class Chat extends React.Component {
   handleSubmit = (message) => {
     axios.post(`http://127.0.0.1:4000/getMessage`,{ message }).then((res) => {
       console.log(res.data);
-      renderCustomComponent(Cheers, {text: res.data.text});
+      console.log("------------------");
+      if(res.data.intent === "PeticionServicio" && res.data.entity === ""){
+        renderCustomComponent(Servicios, {text: res.data.text});
+      }
+      else{renderCustomComponent(Cheers, {text: res.data.text});}
       return res.data;
     });
   };
